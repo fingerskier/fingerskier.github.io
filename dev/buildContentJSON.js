5cc96c13619713cc0a10d3a67ea70c71f5e0c084
fingerskier <fingerskier@gmail.com>
2023-11-11 12:51:14 -0600

    content json object builder

--- dev/buildContentJSON.js
+++ dev/buildContentJSON.js
@@ -1,30 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 
-const contentDirectory = './public/content'; // Adjust this path to your content directory
+const contentDirectory = './public/content'
 
 const isMarkdownFile = fileName => {
-  return path.extname(fileName).toLowerCase() === '.md';
-};
+  return path.extname(fileName).toLowerCase() === '.md'
+}
 
 
 const buildJson = (dir, baseUrl) => {
-  let results = [];
-
+  let results = {}
+  
   fs.readdirSync(dir).forEach(file => {
     const filePath = path.join(dir, file);
     const stat = fs.statSync(filePath);
-
+    
     if (stat && stat.isDirectory()) {
-      results = results.concat(buildJson(filePath, baseUrl));
+      results[file] = buildJson(filePath, baseUrl)
     } else if (isMarkdownFile(file)) {
-      const fileUrl = `${baseUrl}/${path.relative(contentDirectory, filePath).replace(/\\/g, '/')}`;
-      results.push({ path: filePath, url: fileUrl });
-    }
-  });
+      const fileKey = path.basename(file, '.md')
+      const fileUrl = `${baseUrl}/${path.relative(contentDirectory, filePath)}`
 
-  return results;
-};
+      results[fileKey] = {
+        path: filePath.replace(/\\/g, '/'), 
+        url: fileUrl.replace(/\\/g, '/'),
+      }
+    }
+  })
+  
+  return results
+}
 
 
 const jsonContent = buildJson(contentDirectory, '/content');
---
+++ dev/public/content/schlum/test.md
@@ -0,0 +1,1 @@
+# Schlum / Test
